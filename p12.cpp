92c5595d9bef121f6641221dd7ab08192a0c71fb
snowbasak <116074596+snowbasak@users.noreply.github.com>
2022-10-18 15:49:30 +0530

    Update p12.cpp

--- p12.cpp
+++ p12.cpp
@@ -14,20 +14,20 @@ ABCD
 #include<iostream>
  using namespace std;
  int main(){
-    int a,b;
-    cin>>a;
+    int s;
+    cin>>s;
     int count=1;
  
     int i=1;
     char ch= 'A';
-    while(i<=a){
+    while(i<=s){
         int j=1;
         
-        while(j<=a){
+        while(j<=s){
             char ab='A'+j-1;
             
             cout<< ab;
-           // a=a+1;
+           // s=s+1;
             j=j+1;
             
 
