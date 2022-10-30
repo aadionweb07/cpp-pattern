859df9b76f1aa9e870b0911d0debc55c851f9f43
Keshav Kashyap <109139023+Kashyap-1225@users.noreply.github.com>
2022-10-30 21:58:31 +0530

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -1,24 +1,23 @@
- 
-/*        111
-          222
-          333      */
-            
- 
- 
- #include<iostream>
- using namespace std;
- int main(){
-    int a;
-    cin>>a;
-    int i=1;
-    while(i<=a){
-        int j=1;
-        while(j<=a){
-            cout<< i;
-            j=j+1;
-        }
-        cout<<endl;
-        i=i+1;
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    char test;
+    int digits=0, alpha=0, special=0, spaces=0;
+    cout<<"Enter line to check (enter ~ to terminate) ";
+    test=cin.get();
+    while(test!='~')
+    {
+        if(test>='0' && test<='9')
+            digits++;
+        else if((test>='a' && test<='z') || (test>='A' && test<='Z'))
+            alpha++;
+        else if(test==' ' || test=='\n' || test=='\t')
+            spaces++;
+        else special++;
+        test=cin.get();
     }
- }
+    cout<<"Digits\t"<<digits<<endl<<"Alphabets\t"<<alpha<<endl;
+    cout<<"Spaces\t"<<spaces<<endl<<"Special Characters\t"<<special;
+    return 0;
+}
